51a3b2461c133be58f39a1f8e1b7a8719c61fd72
Miguel Tucu Gomez <miguelgomez66@gmail.com>
2021-09-22 18:35:11 -0300

    Add controller

---
+++ server/controllers/posts.js
@@ -0,0 +1,26 @@
+import PostMessage from "../models/postMessage.js"
+
+export const getPosts = async (req, res) => { //This route is reachable through localhost:5000/posts/
+  try {
+    const postMessages = await PostMessage.find();
+    res.status(200).json(postMessages);
+  } catch (error) {
+    res.status(404).json(error.message);
+  }
+
+
+
+  
+}
+
+export const createPost = async (req, res) => { 
+  const post = req.body;
+  const newPost = new PostMessage(post)
+  
+  try {
+    await newPost.save();
+    res.status(201).json(newPost);
+  } catch (error) {
+    res.status(409).json(error.message);
+  }
+}
