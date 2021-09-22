f38b5d4ab41a51da91064722e713adac25996c51
Miguel Tucu Gomez <miguelgomez66@gmail.com>
2021-09-22 20:39:54 -0300

    Add data handling for creating posts

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,18 +1,26 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import Post from "./Post/Post";
+
 
 import useStyles from './styles.js'
 
 const Posts = () => {
-const classes = useStyles();
-  return ( 
-    <div className={classes.mainContainer}> 
-      <h1>Posts</h1>
-      <Post className = {classes.actionDiv} />
-      <Post />
-      <Post />
-    </div>
-   );
+  const posts = useSelector((state) => state.posts)
+  const classes = useStyles();
+
+  console.log(".......");
+  console.log(posts);
+  console.log(".......");
+
+    return ( 
+      <div className={classes.mainContainer}> 
+        <h1>Posts</h1>
+        <Post className = {classes.actionDiv} />
+        <Post />
+        <Post />
+      </div>
+    );
 }
  
 export default Posts;
