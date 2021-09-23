c81a7b82f236e38b0f951826e958b076e5c03a54
Miguel Tucu Gomez <miguelgomez66@gmail.com>
2021-09-23 17:02:35 -0300

    Adjust styling

--- client/src/App.js
+++ client/src/App.js
@@ -29,11 +29,11 @@ const App = () => {
       <Grow in>
         <Container>
           <Grid container justify="space-between" alignItems="stretch" spacing={4}>
-            <Grid item xs={12} sm={7}>
-              <Posts setCurrentId={setCurrentId} setLikeOrDeleted={setLikeOrDeleted} likeOrDeleted={likeOrDeleted} />
-            </Grid>
              <Grid item xs={12} sm={4}>
                <Form currentId={currentId} setCurrentId={setCurrentId} />
+            </Grid>
+            <Grid item xs={12} sm={7}>
+              <Posts setCurrentId={setCurrentId} setLikeOrDeleted={setLikeOrDeleted} likeOrDeleted={likeOrDeleted} />
             </Grid>
           </Grid>
         </Container>
