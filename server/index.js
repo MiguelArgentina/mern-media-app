8284535188eec944882e157381b8ec2c263fc76b
Miguel Tucu Gomez <miguelgomez66@gmail.com>
2021-09-23 17:15:48 -0300

    Add dotenv for managing confidencial data

--- server/index.js
+++ server/index.js
@@ -3,8 +3,10 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import postsRoutes from './routes/posts.js';
+import dotenv from 'dotenv';
 
 const app = express();
+dotenv.config();
 
 
 app.use(bodyParser.json({limit: "30mb", extended: true}));
@@ -13,14 +15,9 @@ app.use(cors());
 
 app.use('/posts', postsRoutes); //This adds the prefix posts to all the routes handled in postRoutes
 
-const CONNECTION_URL = "mongodb+srv://tucu:tucu123@cluster0.jnvvm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5001;
 
-const PORT = process.env.PORT || 5000;
 
-//const mongooseOptions = { useNewUrlParser: true, useUnifiedTopology: true }
-
-mongoose.connect(CONNECTION_URL)
+mongoose.connect(process.env.CONNECTION_URL)
   .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
   .catch((error) => console.log(error.message));
-
-  //mongoose.set('useFindAndModify', false);
