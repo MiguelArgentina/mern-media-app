448ea62cef3e3be3c156410e4ea859c2d1cf45d7
Miguel Tucu Gomez <miguelgomez66@gmail.com>
2021-09-22 21:30:56 -0300

    Add route and controller for updating a post (server)

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,4 @@
+import { Mongoose } from "mongoose";
 import PostMessage from "../models/postMessage.js"
 
 export const getPosts = async (req, res) => { //This route is reachable through localhost:5000/posts/
@@ -20,6 +21,20 @@ export const createPost = async (req, res) => {
   try {
     await newPost.save();
     res.status(201).json(newPost);
+  } catch (error) {
+    res.status(409).json(error.message);
+  }
+}
+
+export const updatePost = async (req, res) => { 
+  const { id: _id } = req.params;
+  const post = req.body;
+
+  if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post matches the requested id');
+
+  try {
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, { new: true });
+    res.json(updatedPost);
   } catch (error) {
     res.status(409).json(error.message);
   }
